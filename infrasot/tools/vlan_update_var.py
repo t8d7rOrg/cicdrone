2ce61c78b44b0e71b19876ff6e06adedd3b92b75
t8d7r <t8d7r@protonmail.com>
2025-01-29 14:06:29 +0000

    fix

--- infrasot/tools/vlan_update_var.py
+++ infrasot/tools/vlan_update_var.py
@@ -1,39 +1,47 @@
-import git
-import difflib
-import hashlib
+import sys
+import os
+import yaml
+import subprocess
+import jinja2
+from jinja2 import Template
+import pandas
+import numpy as np
+from suzieq.sqobjects import get_sqobject
+from rich import print
 
-def compare_git_file_with_local(repo_path: str, file_path: str, last_state_path:str):
-    try:
-        g = git.Git(repo_path)
-        repo = git.Repo(repo_path)
-        commit_count = 0
-        
-        # Récupérer la liste des commits affectant le fichier
-        hexshas = g.log('--pretty=%H', '--follow', '--', file_path).split('\n')
-        
-        # Ouvrir le fichier d'état actuel
-        with open(last_state_path, 'r', encoding='utf-8') as last_state:
-            last_state_content = last_state.read()
-        hash_last_state = hashlib.md5(last_state_content.encode('utf-8')).hexdigest()
-        
-        # Comparer avec les versions du fichier dans Git
-        for commit_hash in hexshas:
-            commit = repo.commit(commit_hash)
-            file_content = commit.tree[file_path].data_stream.read().decode('utf-8')
-            hash_commit = hashlib.md5(file_content.encode('utf-8')).hexdigest()
-            
-            if hash_last_state == hash_commit:
-                print(f"Le fichier local et la version du commit {commit_hash} sont identiques et vous êtes à"+str(commit_count)+" commit de l etat actuel")
-                break
-            else:
-                commit_count +=1
-                print(f"Le fichier local et la version du commit {commit_hash} sont différents.")
-    except Exception as e:
-        print(f"Erreur : {e}")
+old_yaml_path = "/drone/src/nfrasot/archive/vlan_list_last_state.yml"
+new_yaml_path = "/drone/src/infrasot/vlan_list.yml"
+path_template = "/drone/src/ansible-material/template"
+group_vars_path = "/done/src/ansible-material/group_vars/"
 
-# Exemple d'utilisation
-repo_path = "/home/tom87/cicdrone/"  # Remplacez par votre dépôt Git
-file_path = "infrasot/vlan_list.yml"  # Remplacez par le fichier à comparer
-last_state_path = "/home/tom87/cicdrone/infrasot/archive/vlan_list_last_state.yml"
+def load_yaml_file(path):
+    with open(path, 'r') as stream:
+        dict = yaml.safe_load(stream)
+    return dict
 
-compare_git_file_with_local(repo_path, file_path, last_state_path)
+old_vlans = load_yaml_file(old_yaml_path)
+new_vlans = load_yaml_file(new_yaml_path)
+
+vlan_list_to_be_created = []
+vlan_list_to_be_deleted = []
+
+#Check if new vlans in vlan state are in the previous state :
+for vlan in new_vlans["vlans_list"].keys():
+    if vlan not in old_vlans["vlans_list"].keys():
+        if vlan not in vlan_list_to_be_created:
+            vlan_list_to_be_created.append(vlan)
+
+#Check if old vlans in vlan state are still in the new state :
+for vlan in old_vlans["vlans_list"].keys():
+    if vlan not in new_vlans["vlans_list"].keys():
+        if vlan not in vlan_list_to_be_deleted:
+            vlan_list_to_be_deleted.append(vlan)
+
+if (not vlan_list_to_be_created and not vlan_list_to_be_deleted):
+    print ("No change")
+else :
+    print ("Following vlans should be created :",vlan_list_to_be_created)
+    print ("Following vlans shoudl be deleted :",vlan_list_to_be_deleted)
+
+vgf = jinja2.Environment(loader=jinja2.FileSystemLoader(path_template)).get_template('vlan_update.j2').render(vlans = vlan_list_to_be_created)
+with open(group_vars_path+"Leaves.yaml",'w') as f: f.write(vgf)
